5c703eab2f928f016776d7b2b215c58fbf5c274f
Henrique Daltrozo <henriquedaltrozo3@gmail.com>
2025-05-27 18:37:32 -0300

    chore: initial typeorm configuration

--- src/common/infrastructure/http/server.ts
+++ src/common/infrastructure/http/server.ts
@@ -1,7 +1,15 @@
 import { env } from '../env'
+import { dataSource } from '../typeorm'
 import { app } from './app'
 
-app.listen(env.PORT, () => {
-  console.log(`Server is running on http://localhost:${env.PORT}`)
-  console.log(`API docs available at http://localhost:${env.PORT}/docs`)
-})
+dataSource
+  .initialize()
+  .then(() => {
+    app.listen(env.PORT, () => {
+      console.log(`Server is running on http://localhost:${env.PORT}`)
+      console.log(`API docs available at http://localhost:${env.PORT}/docs`)
+    })
+  })
+  .catch(error => {
+    console.error('Error during Data Source initialization:', error)
+  })
